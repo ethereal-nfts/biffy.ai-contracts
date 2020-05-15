f82b55fa4080021932d39f6c5035545d756b7e16
Ethereal <0_ethereal_0@protonmail.com>
2020-05-15 01:14:17 -0700

    Fix compile errors

--- contracts/BastetRitual.sol
+++ contracts/BastetRitual.sol
@@ -25,36 +25,37 @@ contract BastetRitual is Initializable {
     mapping(address => mapping(uint => uint)) public participantSacrificeEther;
     mapping(uint => uint) public sacrificeEther;
 
-    modifier isAnyLobbyActive() {
+    modifier isAnySacrificeActive() {
         require(
         loveCycle.hasStarted(),
         "The first Sacrifice starts at the begining of the LoveCycle."
         );
         require(
-        loveCycle.daysSinceCycleStart() < lobbyCount,
+        loveCycle.daysSinceCycleStart() < sacrificeCount,
         "Final Sacrifice has ended."
-        )
+        );
+        _;
     }
 
     function initialize(
         BiffyLovePoints _biffyLovePoints,
         UniswapExchangeInterface _uniswapExchange,
-        LoveCYcle _loveCycle,
-        uint _lobbySize,
-        uint _lobbyTime,
-        uint _lobbyCount,
+        LoveCycle _loveCycle,
+        uint _sacrificeSize,
+        uint _sacrificeTime,
+        uint _sacrificeCount,
         uint _unclaimedDecayBP
     ) public initializer {
         biffyLovePoints = _biffyLovePoints;
         uniswapExchange = _uniswapExchange;
         loveCycle = _loveCycle;
-        lobbySize = _lobbySize;
-        lobbyTime = _lobbyTime;
-        lobbyCount = _lobbyCount;
+        sacrificeSize = _sacrificeSize;
+        sacrificeTime = _sacrificeTime;
+        sacrificeCount = _sacrificeCount;
         unclaimedDecayBP = _unclaimedDecayBP;
     }
 
-    function sacrificeForLove() public payable isAnyLobbyActive {
+    function sacrificeForLove() public payable isAnySacrificeActive {
         require(msg.value > 0, "Sacrifice Ether to earn Love.");
         uint currentSacrifice = loveCycle.daysSinceCycleStart();
         participantSacrificeEther[msg.sender][currentSacrifice] =
@@ -62,6 +63,4 @@ contract BastetRitual is Initializable {
         sacrificeEther[currentSacrifice] =
             sacrificeEther[currentSacrifice].add(msg.value);
     }
-
-    
 }
