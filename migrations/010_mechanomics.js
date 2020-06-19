234ff62d23b9df2b307d4e103ddb6bad4c78f5bc
Ethereal <0_ethereal_0@protonmail.com>
2020-06-18 22:01:08 -0700

    Alter migrations to follow Biffy's phases. Add Mechanomics phase.

---
+++ migrations/010_mechanomics.js
@@ -0,0 +1,40 @@
+const { scripts, ConfigManager } = require("@openzeppelin/cli")
+const { add, push, create } = scripts
+
+const config = require("../config")
+
+const BiffyLovePoints = artifacts.require("BiffyLovePoints")
+const BastetsExchange = artifacts.require("BastetsExchange")
+
+async function deploy(options) {
+  add({ contractsData: [{ name: "BiffyLovePoints", alias: "BiffyLovePoints" },{ name: "BastetsExchange", alias: "BastetsExchange" }] })
+  await push(options)
+  await create(Object.assign({ contractAlias: "BiffyLovePoints" }, options))
+  await create(Object.assign({ contractAlias: "BastetsExchange" }, options))
+}
+
+async function initialize(accounts) {
+  const loveParams = config.InitializationBiffyLovePoints
+  const biffyLovePoints = await BiffyLovePoints.deployed()
+  await biffyLovePoints.initialize(
+    loveParams.name,
+    loveParams.symbol,
+    loveParams.decimals,
+    loveParams.minters.map((index)=>accounts[index]),
+    loveParams.pausers.map((index)=>accounts[index])
+  )
+
+  const bastetsExchange = await BastetsExchange.deployed()
+  await bastetsExchange.initialize(
+    biffyLovePoints.address
+  )
+
+}
+
+module.exports = function(deployer, networkName, accounts) {
+  deployer.then(async () => {
+    const { network, txParams } = await ConfigManager.initNetworkConfiguration({ network: networkName, from: accounts[0] })
+    await deploy({ network, txParams })
+    await initialize(accounts)
+  })
+}
